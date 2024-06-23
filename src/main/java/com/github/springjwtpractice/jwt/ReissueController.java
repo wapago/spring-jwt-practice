4c37dae3ee3e8fe6a2ef64a41c136c2658c380af
Kim Chanho <beroso20th@gmail.com>
2024-06-23 18:15:56 +0900

    Feat: refresh rotate

--- src/main/java/com/github/springjwtpractice/jwt/ReissueController.java
+++ src/main/java/com/github/springjwtpractice/jwt/ReissueController.java
@@ -53,10 +53,20 @@ public class ReissueController {
 
         //make new JWT
         String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
+        String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L); // 24시간
 
         //response
         response.setHeader("access", newAccess);
+        response.addCookie(createCookie("refresh", newRefresh));
 
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private Cookie createCookie(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24*60*60);
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 }
